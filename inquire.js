ab70c80bce581ba5d08d129411cd3bbe492d43d2
goofy foot <williamk31@gmail.com>
2024-03-21 17:57:08 -0600

    fixed view employees SQL query

--- inquire.js
+++ inquire.js
@@ -81,7 +81,7 @@ function viewAllRoles() {
 }
 
 function viewAllEmployees() {
-    const sql = `SELECT * FROM employee`
+    const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS "manager" FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN employee manager ON employee.manager_id = manager.id`
     pool.query(sql, (err, { rows }) => {
         if (err) {
             console.log(err);
