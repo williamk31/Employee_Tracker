a039e88dc4bffb07b2564d0bfc2296aeb343e2fa
goofy foot <williamk31@gmail.com>
2024-03-21 18:14:55 -0600

    fixed application flow and formatting

--- inquire.js
+++ inquire.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const { Pool } = require('pg');
+const { start } = require('repl');
 
 const pool = new Pool(
     {
@@ -28,8 +29,8 @@ function response(data) {
     console.log(data.options)
     switch (data.options) {
         case 'View all departments':
-           viewAllDepartments();
-           break;
+            viewAllDepartments();
+            break;
         case 'View all roles':
             viewAllRoles();
             break;
@@ -60,35 +61,38 @@ function startProgram() {
 }
 
 function viewAllDepartments() {
-    console.log('in the function')
     const sql = `SELECT id, name AS department FROM department`
     pool.query(sql, (err, { rows }) => {
         if (err) {
             console.log(err);
-          }
-          console.table(rows);
-    })
-}
+        }
+        console.table(rows);
+        startProgram();
+    });
+
+};
 
 function viewAllRoles() {
     const sql = `SELECT role.id, role.title, role.salary, department.name AS department FROM role LEFT JOIN department ON role.department = department.id`
     pool.query(sql, (err, { rows }) => {
         if (err) {
             console.log(err);
-          }
-          console.table(rows);
-    })
-}
+        }
+        console.table(rows);
+        startProgram();
+    });
+};
 
 function viewAllEmployees() {
     const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS "manager" FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN employee manager ON employee.manager_id = manager.id`
     pool.query(sql, (err, { rows }) => {
         if (err) {
             console.log(err);
-          }
-          console.table(rows);
-    })
-}
+        }
+        console.table(rows);
+        startProgram();
+    });
+};
 
 function addDepartment() {
     inquirer.prompt({
@@ -96,20 +100,23 @@ function addDepartment() {
         message: 'Department Name',
         name: 'department'
     })
-        .then(function(data){ 
+        .then(function (data) {
             console.log(data.department)
             const sql = `INSERT INTO department (name) VALUES ($1)`
             let departmentName = data.department
             pool.query(sql, [departmentName], (err, result) => {
                 if (err) {
-            console.log(err);
-            }
-          console.log(`${data.department} succesfully added to Departments`);
-            })
+                    console.log(err);
+                }
+                console.log(`${data.department} succesfully added to Departments`);
+                viewAllDepartments();
+            });
+
         })
 
+
 }
-    
+
 function addRole() {
     let departmentSql = `SELECT id, name FROM department`;
     pool.query(departmentSql, (err, result) => {
@@ -119,38 +126,40 @@ function addRole() {
         }
         let departments = result.rows;
         let departmentsArray = departments.map(department => department.name)
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'Role Name',
-            name: 'role'
-        },
-        {
-            type: 'input',
-            message: 'Salary',
-            name: 'salary'
-        },
-        {
-            type: 'list',
-            choices: departmentsArray,
-            message: 'What department does this role belong to?',
-            name: 'chosenDepartment'
-        }
-    ])
-        .then(function(data){ 
-            let selectedDepartment = departments.find(department => data.chosenDepartment === department.name);
-    
-            const sql = `INSERT INTO role (title, salary, department) VALUES ($1, $2, $3)`
-            let roleName = data.role
-            let roleSalary = data.salary
-            pool.query(sql, [roleName, roleSalary, selectedDepartment.id], (err, result) => {
-                if (err) {
-                    console.log(err);
-                    return;
-                 }
-          console.log(`${data.role} succesfully added to Roles!`);
+        inquirer.prompt([
+            {
+                type: 'input',
+                message: 'Role Name',
+                name: 'role'
+            },
+            {
+                type: 'input',
+                message: 'Salary',
+                name: 'salary'
+            },
+            {
+                type: 'list',
+                choices: departmentsArray,
+                message: 'What department does this role belong to?',
+                name: 'chosenDepartment'
+            }
+        ])
+            .then(function (data) {
+                let selectedDepartment = departments.find(department => data.chosenDepartment === department.name);
+
+                const sql = `INSERT INTO role (title, salary, department) VALUES ($1, $2, $3)`
+                let roleName = data.role
+                let roleSalary = data.salary
+                pool.query(sql, [roleName, roleSalary, selectedDepartment.id], (err, result) => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                    console.log(`${data.role} succesfully added to Roles!`);
+                    viewAllRoles();
+                })
+
             })
-        })
     })
 
 }
@@ -169,48 +178,49 @@ function addEmployee() {
             if (err) {
                 console.log(err);
                 return;
-            } 
-            console.log(result);
+            }
             let employees = result.rows;
             let employeeNames = employees.map(employee => `${employee.first_name} ${employee.last_name}`);
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'Employee First Name',
-            name: 'firstName'
-        },
-        {
-            type: 'input',
-            message: 'Employee Last Name',
-            name: 'lastName'
-        },
-        {
-            type: 'list',
-            choices: rolesArray,
-            message: 'What is this employees role?',
-            name: 'role'
-        },
-        {
-            type: 'list',
-            choices: employeeNames,
-            message: 'Who is their manager?',
-            name: 'manager'
-        }
-    ])
-        .then(function(data){ 
-            let selectedRole = roles.find(role => data.role === role.title);
-            let selectedManager = employees.find(employee => data.manager === `${employee.first_name} ${employee.last_name}`);
-            let firstName = data.firstName;
-            let lastName = data.lastName;
-            const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)`
-            pool.query(sql, [firstName, lastName, selectedRole.id, selectedManager.id], (err, result) => {
-                if (err) {
-            console.log(err);
-            }
-          console.log(`${firstName} ${lastName} succesfully added to Employees`);
-            })
-        })
-    });
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    message: 'Employee First Name',
+                    name: 'firstName'
+                },
+                {
+                    type: 'input',
+                    message: 'Employee Last Name',
+                    name: 'lastName'
+                },
+                {
+                    type: 'list',
+                    choices: rolesArray,
+                    message: `What is this employee's role?`,
+                    name: 'role'
+                },
+                {
+                    type: 'list',
+                    choices: employeeNames,
+                    message: `Who is this employee's manager?`,
+                    name: 'manager'
+                }
+            ])
+                .then(function (data) {
+                    let selectedRole = roles.find(role => data.role === role.title);
+                    let selectedManager = employees.find(employee => data.manager === `${employee.first_name} ${employee.last_name}`);
+                    let firstName = data.firstName;
+                    let lastName = data.lastName;
+                    const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)`
+                    pool.query(sql, [firstName, lastName, selectedRole.id, selectedManager.id], (err, result) => {
+                        if (err) {
+                            console.log(err);
+                        }
+                        console.log(`${firstName} ${lastName} succesfully added to Employees`);
+                        viewAllEmployees();
+                    })
+
+                })
+        });
     });
 
 }
@@ -221,57 +231,58 @@ function updateEmployeeRole() {
         if (err) {
             console.log(err);
             return;
-        } 
-        console.log(result);
+        }
         let employees = result.rows;
         let employeeNames = employees.map(employee => `${employee.first_name} ${employee.last_name}`);
 
         let roleSql = `SELECT id, title FROM role`;
         pool.query(roleSql, (err, result) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        let roles = result.rows;
-        let rolesArray = roles.map(role => role.title);
-   
-    inquirer.prompt([
-        {
-            type: 'list',
-            choices: employeeNames,
-            name: 'employees',
-            message: 'Choose an employee to update:'
-        },
-        {
-            type: 'list',
-            choices: rolesArray,
-            name: 'roles',
-            message: 'What is their new role?'
-        }
-    ])
-        .then(function(data){
-            
-            let newRoleID;
-            roles.forEach((role) => {
-                if(data.roles === role.title){
-                    newRoleID = role.id
+            if (err) {
+                console.log(err);
+                return;
+            }
+            let roles = result.rows;
+            let rolesArray = roles.map(role => role.title);
+
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    choices: employeeNames,
+                    name: 'employees',
+                    message: 'Choose an employee to update:'
+                },
+                {
+                    type: 'list',
+                    choices: rolesArray,
+                    name: 'roles',
+                    message: 'What is their new role?'
                 }
-            })
-            let employeeID;
-            employees.forEach((employee) => {
-                if(data.employees === `${employee.first_name} ${employee.last_name}`){
-                    employeeID = employee.id
-                }
-            })
-            const sql = `UPDATE employee SET role_id = $1 WHERE id = $2`;
-            pool.query(sql, [newRoleID, employeeID], (err, result) => {
-                if (err) {
-                    console.log(err);
-                }
-                console.log(`Employee's role updated succesfully!`)
-            });
-        })
-    });
+            ])
+                .then(function (data) {
+
+                    let newRoleID;
+                    roles.forEach((role) => {
+                        if (data.roles === role.title) {
+                            newRoleID = role.id
+                        }
+                    })
+                    let employeeID;
+                    employees.forEach((employee) => {
+                        if (data.employees === `${employee.first_name} ${employee.last_name}`) {
+                            employeeID = employee.id
+                        }
+                    })
+                    const sql = `UPDATE employee SET role_id = $1 WHERE id = $2`;
+                    pool.query(sql, [newRoleID, employeeID], (err, result) => {
+                        if (err) {
+                            console.log(err);
+                        }
+                        console.log(`Employee's role updated succesfully!`);
+                        viewAllEmployees();
+                    });
+
+                })
+        });
     });
 }
 
